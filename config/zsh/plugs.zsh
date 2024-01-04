fa7f1795aedca5bc25d51f050099d0e71748c7a1
Aniruddh <aniruddhganesh@protonmail.com>
2024-01-04 20:19:00 +0530

    Fixed security

--- config/zsh/plugs.zsh
+++ config/zsh/plugs.zsh
@@ -1,4 +1,2 @@
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-export SPOTIPY_CLIENT_ID='a41ec911969348c399ee3e8c72282966'
-export SPOTIPY_CLIENT_SECRET='357cb12a1c294c46958975de6efe6dce'
