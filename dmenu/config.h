541ebb4779d9c31caf0e5223e8d6097e686b38cb
Aniruddh <aniruddhganesh@protonmail.com>
2024-02-09 20:10:48 +0530

    Added suckless software for desktop

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Terminess Nerd Font:size=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#01a1cd", "#0d0320" },
+	[SchemeSel] = { "#73da9e", "#0f0326" },
+	[SchemeOut] = { "#73DA9E", "#CD8af3" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
---
+++ st/README.md
@@ -0,0 +1,11 @@
+# st - simple terminal
+--------------------
+st is a simple terminal emulator for X which sucks less.
+
+## Patches applied
+- Ligatures Scrollback
+
+Credits
+-------
+Based on Aurélien APTEL <aurelien dot aptel at gmail dot com> bt source code.
+
